277da29782db3542161bf7918bed2557e218143e
zenvxous <zenvxous@gmail.com>
2025-02-17 21:40:40 +0300

    check-style fix

--- src/main/java/ordertracker/api/controllers/MealController.java
+++ src/main/java/ordertracker/api/controllers/MealController.java
@@ -2,7 +2,11 @@ package ordertracker.api.controllers;
 
 import java.util.List;
 import ordertracker.core.models.Meal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
